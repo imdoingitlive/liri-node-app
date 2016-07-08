48cfca91ac2b49522226e6d0fe03d0e898c6a10d
Edwin Rivera <edwin.rivera@outlook.com>
2016-07-08 10:43:55 -0400

    added movie-this functionality

--- liri.js
+++ liri.js
@@ -1,7 +1,11 @@
-var keys = require('keys.js');
+//var keys = require('keys.js');
+var request = require('request');
+var args = process.argv;
+var command = process.argv[2];
 
-//install npm packages twitter, spotify, request
-  //request will call the OMDB API
+var movieName = "";
+
+//request will call the OMDB API
 
 //take in argvs 'my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'
 
@@ -14,7 +18,47 @@ var keys = require('keys.js');
 //movie-this 'movie name here' will show
   //title, year, imdb rating, country, language, plot, actors, rotten tomatoes rating, RT URL
   //no movie provided default to 'Mr. Nobody'
+  //RT rating and URL are now OPTIONAL
+if (command === "movie-this"){
 
+  for (var i=3; i<args.length; i++){
+    if (i>3 && i< args.length){
+      movieName = movieName + "+" + args[i];
+    }
+    else {
+      movieName = movieName + args[i];
+    }
+  }
+
+  var queryUrl = 'http://www.omdbapi.com/?t=' + movieName +'&y=&plot=short&r=json';
+
+  request(queryUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log("Title: " + JSON.parse(body)["Title"]);
+      console.log("Release Year: " + JSON.parse(body)["Year"]);
+      console.log("IMDB Rating: " + JSON.parse(body)["imdbRating"]);
+      console.log("Country: " + JSON.parse(body)["Country"]);
+      console.log("Language: " + JSON.parse(body)["Language"]);
+      console.log("Plot: " + JSON.parse(body)["Plot"]);
+      console.log("Actors: " + JSON.parse(body)["Actors"]);
+    }
+  });
+}
+else {
+  var queryUrl = 'http://www.omdbapi.com/?t=mr.%20nobody&y=&plot=short&r=json';
+
+  request(queryUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log("Title: " + JSON.parse(body)["Title"]);
+      console.log("Release Year: " + JSON.parse(body)["Year"]);
+      console.log("IMDB Rating: " + JSON.parse(body)["imdbRating"]);
+      console.log("Country: " + JSON.parse(body)["Country"]);
+      console.log("Language: " + JSON.parse(body)["Language"]);
+      console.log("Plot: " + JSON.parse(body)["Plot"]);
+      console.log("Actors: " + JSON.parse(body)["Actors"]);
+    }
+  });
+}
 //do-what-it-says should use the fs node package to take the text inside of random.txt and use it to call the first command with the second part as it's parameter
   //so call the appropriate function and pass in "I Want it That Way" as the song
   //this should work for any function and parameter I use
